7a363e7b63fdc419b1227ebe9a781f557cabfb15
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-06-25 22:47:54 +0300

    Update cert_gen.php

--- cert_gen.php
+++ cert_gen.php
@@ -1,7 +1,13 @@
 <?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+header("Content-Type: text/html; charset=utf-8");
 session_start();
 
-if(!empty(session_id($_COOKIE['session_id']))); 
+if(!empty($_COOKIE['session_id'])) {
+    session_id($_COOKIE['session_id']);
+}
 
 
 if (empty($_SESSION["name"])) {
